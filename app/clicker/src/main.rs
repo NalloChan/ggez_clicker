92a8217558eb81290f9d3895453dcd6413e3acde
NalloChan <75219485+NalloChan@users.noreply.github.com>
2024-03-10 12:44:01 +0900

    ggezスタートアップ

--- app/clicker/src/main.rs
+++ app/clicker/src/main.rs
@@ -1,3 +1,44 @@
+use ggez::conf::{Conf, WindowMode, WindowSetup};
+use ggez::event::{self, EventHandler};
+use ggez::graphics::{Canvas, Color};
+use ggez::{Context, ContextBuilder, GameError, GameResult};
+
+struct MainState {}
+
+impl EventHandler<GameError> for MainState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        const FPS_LIMIT: u32 = 100;
+
+        while ctx.time.check_update_time(FPS_LIMIT) {}
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        let canvas = Canvas::from_frame(ctx, Color::from([0.1, 0.1, 0.1, 1.0]));
+        canvas.finish(ctx)?;
+
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let state = MainState {};
+
+    let config = Conf::new();
+    let (ctx, event_loop) = ContextBuilder::new("clicker_game", "me")
+        .default_conf(config)
+        .window_setup(WindowSetup {
+            title: "Clicker Game".to_string(),
+            ..Default::default()
+        })
+        .window_mode(WindowMode {
+            width: 1600.0,
+            height: 900.0,
+            ..Default::default()
+        })
+        .build()
+        .unwrap();
+
+    event::run(ctx, event_loop, state);
 }
